99a0e0275c88bc272263a334151464f4162af078
LucaJahnen <luca.jahnen@gmail.com>
2024-08-19 14:17:13 +0200

    implementing result section

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const modal = document.querySelector(".modal");
+const modal = document.querySelector(".modal") || null;
 const rulesButton = document.querySelector(".rules");
 const closeButton = document.querySelector(".close");
 rulesButton === null || rulesButton === void 0 ? void 0 : rulesButton.addEventListener("click", () => {
@@ -18,49 +18,99 @@ closeButton === null || closeButton === void 0 ? void 0 : closeButton.addEventLi
     modal === null || modal === void 0 ? void 0 : modal.close();
 });
 const cards = document.querySelectorAll(".card");
-const wrappers = document.querySelectorAll(".wrapper");
-const bgTriangle = document.querySelector(".bg-triangle");
-const headings = document.querySelectorAll(".heading");
 const possibilites = ["paper", "scissors", "rock"];
 const main = document.querySelector("main");
-cards.forEach((card, index) => {
-    card.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
-        main === null || main === void 0 ? void 0 : main.innerHTML = "";
-        const houseChoice = possibilites[Math.floor(Math.random() * 3)];
-        const wait = (ms) => {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        };
-        const getHouseChoice = () => __awaiter(void 0, void 0, void 0, function* () {
-            var _a;
-            yield wait(2000);
-            (_a = document.querySelector(".house-wrapper")) === null || _a === void 0 ? void 0 : _a.innerHTML += `
-            <div class="wrapper" style="transform: translate(4%, -104%) scale(calc(28 / 26))">
+const handleClick = (index) => {
+    console.log("click");
+    let mainHTML;
+    if (main) {
+        mainHTML = main.innerHTML;
+        main.innerHTML = "";
+    }
+    // prevent a draw
+    const housePossibilites = possibilites.filter(item => item !== possibilites[index]);
+    const houseChoice = housePossibilites[Math.floor(Math.random() * 2)];
+    const wait = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    };
+    const getHouseChoice = () => __awaiter(void 0, void 0, void 0, function* () {
+        yield wait(2000);
+        const houseWrapper = document.querySelector(".house-wrapper");
+        if (houseWrapper) {
+            houseWrapper.innerHTML += `
+            <div class="wrapper" style="transform: translate(4%, -108%) scale(1.5)">
                 <button class="card ${houseChoice}" style="left: auto; top: auto">
                     <img src="images/icon-${houseChoice}.svg" alt="">
                 </button>
             </div>
             `;
-        });
-        const el = `
-        <div class="choice-wrapper">
-            <h2 class="heading you">you picked</h2>
-            <div class="wrapper" style="transform: scale(1.5)">
-                <button class="card ${possibilites[index]}" style="left: auto; top: auto">
-                    <img src="images/icon-${possibilites[index]}.svg" alt="">
-                </button>
-            </div>
+        }
+    });
+    const el = `
+    <div class="choice-wrapper">
+        <h2 class="heading you">you picked</h2>
+        <div class="wrapper" style="transform: scale(1.5)">
+            <button class="card ${possibilites[index]}" style="left: auto; top: auto">
+                <img src="images/icon-${possibilites[index]}.svg" alt="">
+            </button>
         </div>
-        <div class="choice-wrapper">
-            <h2 class="heading house">the house picked</h2>
-            <div class="house-wrapper">
-                <div class="circle"></div>
-            </div>
+    </div>
+    <div class="choice-wrapper">
+        <h2 class="heading house">the house picked</h2>
+        <div class="house-wrapper">
+            <div class="circle"></div>
         </div>
+    </div>
+    `;
+    getHouseChoice();
+    main === null || main === void 0 ? void 0 : main.insertAdjacentHTML("afterbegin", el);
+    if (main) {
+        main.style.display = "flex";
+        main.style.justifyContent = "center";
+        main.style.gap = "10rem";
+    }
+    const displayResult = () => __awaiter(void 0, void 0, void 0, function* () {
+        var _a;
+        const winCondition = {
+            "paper,rock": 1,
+            "paper,scissors": 0,
+            "rock,paper": 0,
+            "rock,scissors": 1,
+            "scissors,rock": 0,
+            "scissors,paper": 1
+        };
+        const win = winCondition[`${possibilites[index]},${houseChoice}`] === 1;
+        yield wait(2300);
+        const firstChild = main === null || main === void 0 ? void 0 : main.firstElementChild;
+        const message = document.createElement("div");
+        message.classList.add("result");
+        const buttonReset = () => {
+            const button = document.createElement("button");
+            button.textContent = "Play Again";
+            button.addEventListener("click", () => {
+                if (main) {
+                    main.innerHTML = mainHTML;
+                    main.style.display = "block";
+                    document.querySelectorAll(".card").forEach((card, index) => {
+                        console.log(card);
+                        card.addEventListener("click", () => {
+                            handleClick(index);
+                        });
+                    });
+                }
+            });
+            return button;
+        };
+        message.innerHTML = `
+        <h1>${win ? "You Win" : "You Lose"}</h1>
         `;
-        getHouseChoice();
-        main === null || main === void 0 ? void 0 : main.insertAdjacentHTML("afterbegin", el);
-        main === null || main === void 0 ? void 0 : main.style.display = "flex";
-        main === null || main === void 0 ? void 0 : main.style.justifyContent = "center";
-        main === null || main === void 0 ? void 0 : main.style.gap = "10rem";
-    }));
+        message.appendChild(buttonReset());
+        main === null || main === void 0 ? void 0 : main.insertBefore(message, (_a = firstChild === null || firstChild === void 0 ? void 0 : firstChild.nextSibling) !== null && _a !== void 0 ? _a : null);
+    });
+    displayResult();
+};
+cards.forEach((card, index) => {
+    card.addEventListener("click", () => {
+        handleClick(index);
+    });
 });
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-const modal = <HTMLElement>document.querySelector(".modal")
-const rulesButton = <HTMLElement>document.querySelector(".rules")
-const closeButton = <HTMLElement>document.querySelector(".close")
+const modal = document.querySelector(".modal") as HTMLDialogElement || null
+const rulesButton = <HTMLButtonElement>document.querySelector(".rules")
+const closeButton = <HTMLButtonElement>document.querySelector(".close")
 
 rulesButton?.addEventListener("click", () => {
     modal?.showModal()
@@ -11,52 +11,106 @@ closeButton?.addEventListener("click", () => {
 })
 
 const cards = document.querySelectorAll(".card")
-const wrappers = document.querySelectorAll(".wrapper")
-const bgTriangle = document.querySelector(".bg-triangle")
-const headings = document.querySelectorAll(".heading")
 const possibilites = ["paper", "scissors", "rock"]
 const main = document.querySelector("main")
 
-cards.forEach((card, index) => {
-    card.addEventListener("click", async () => {
-        main?.innerHTML = ""
-        const houseChoice = possibilites[Math.floor(Math.random() * 3)]
+const handleClick = (index: number) => {
+    console.log("click")
+    let mainHTML: string
+    if(main) {
+        mainHTML = main.innerHTML
+        main.innerHTML = ""
+    }
+    // prevent a draw
+    const housePossibilites = possibilites.filter(item => item !== possibilites[index])
+    const houseChoice = housePossibilites[Math.floor(Math.random() * 2)]
 
-        const wait = (ms: number) => {
-            return new Promise(resolve => setTimeout(resolve, ms))
-        }
+    const wait = (ms: number) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
 
-        const getHouseChoice = async () => {
-            await wait(2000)
-            document.querySelector(".house-wrapper")?.innerHTML += `
-            <div class="wrapper" style="transform: translate(4%, -104%) scale(calc(28 / 26))">
+    const getHouseChoice = async () => {
+        await wait(2000)
+        const houseWrapper = document.querySelector(".house-wrapper")
+        if(houseWrapper) {
+            houseWrapper.innerHTML += `
+            <div class="wrapper" style="transform: translate(4%, -108%) scale(1.5)">
                 <button class="card ${houseChoice}" style="left: auto; top: auto">
                     <img src="images/icon-${houseChoice}.svg" alt="">
                 </button>
             </div>
             `
         }
+    }
 
-        const el = `
-        <div class="choice-wrapper">
-            <h2 class="heading you">you picked</h2>
-            <div class="wrapper" style="transform: scale(1.5)">
-                <button class="card ${possibilites[index]}" style="left: auto; top: auto">
-                    <img src="images/icon-${possibilites[index]}.svg" alt="">
-                </button>
-            </div>
+    const el = `
+    <div class="choice-wrapper">
+        <h2 class="heading you">you picked</h2>
+        <div class="wrapper" style="transform: scale(1.5)">
+            <button class="card ${possibilites[index]}" style="left: auto; top: auto">
+                <img src="images/icon-${possibilites[index]}.svg" alt="">
+            </button>
         </div>
-        <div class="choice-wrapper">
-            <h2 class="heading house">the house picked</h2>
-            <div class="house-wrapper">
-                <div class="circle"></div>
-            </div>
+    </div>
+    <div class="choice-wrapper">
+        <h2 class="heading house">the house picked</h2>
+        <div class="house-wrapper">
+            <div class="circle"></div>
         </div>
+    </div>
+    `
+    getHouseChoice()
+    main?.insertAdjacentHTML("afterbegin", el)
+    if(main) {
+        main.style.display = "flex"
+        main.style.justifyContent = "center"
+        main.style.gap = "10rem"
+    }
+    
+    const displayResult = async () => {
+        const winCondition: { [key: string]: number } = {
+            "paper,rock": 1,
+            "paper,scissors": 0,
+            "rock,paper": 0,
+            "rock,scissors": 1,
+            "scissors,rock": 0,
+            "scissors,paper": 1
+        }
+        const win = winCondition[`${possibilites[index]},${houseChoice}`] === 1
+        await wait(2300)
+        const firstChild = main?.firstElementChild
+        const message = document.createElement("div")
+        message.classList.add("result")
+
+        const buttonReset = () => {
+            const button = document.createElement("button")
+            button.textContent = "Play Again"
+            button.addEventListener("click", () => {
+                if(main) {
+                    main.innerHTML = mainHTML
+                    main.style.display = "block"
+                    document.querySelectorAll(".card").forEach((card, index) => {
+                        console.log(card)
+                        card.addEventListener("click", () => {
+                            handleClick(index)
+                        })
+                    })
+                }
+            })
+            return button
+        }
+
+        message.innerHTML = `
+        <h1>${win ? "You Win" : "You Lose"}</h1>
         `
-        getHouseChoice()
-        main?.insertAdjacentHTML("afterbegin", el)
-        main?.style.display = "flex"
-        main?.style.justifyContent = "center"
-        main?.style.gap = "10rem"
+        message.appendChild(buttonReset())
+        main?.insertBefore(message, firstChild?.nextSibling ?? null)
+    }
+    displayResult()
+}
+
+cards.forEach((card, index) => {
+    card.addEventListener("click", () => {
+        handleClick(index)
     })
 })
